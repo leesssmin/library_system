aaac7d8a50cf6956535fdbfbe43b347be76e0178
lsssmin <tmxjel77777@naver.com>
2025-05-19 15:01:50 +0900

    student dao

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -15,10 +15,10 @@ public class StudentDAO {
         String sql = "INSERT INTO student (name, student_id)" +
                 "VALUES ( ?, ?)";
         try(Connection conn = DatabaseUtil.getconnection();
-        PreparedStatement pstmt = conn.prepareStatement(sql)) {
-        pstmt.setString(1, student.getName());
-        pstmt.setString(2, student.getStudentId());
-        pstmt.executeUpdate();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, student.getName());
+            pstmt.setString(2, student.getStudentId());
+            pstmt.executeUpdate();
         }
 
 
@@ -26,27 +26,40 @@ public class StudentDAO {
     // 모든 학생 목록을 조회하는 기능
     public List<Student> getAllStudent() throws SQLException{
         List<Student> studentList = new ArrayList<>();
-        String sql = "SELECT * FROM student ";
+        String sql = "SELECT * FROM students ";
         try(Connection conn = DatabaseUtil.getconnection();
-            Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-            String name = rs.getString("name");
-            String studentId= rs.getString("student_id");
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
 
-            Student student = new Student(name, studentId);
-            studentList.add(student);
-
+            while (rs.next()){
+                Student studentDto = new Student();
+                studentDto.setId(rs.getInt("id"));
+                studentDto.setName(rs.getString("name"));
+                studentDto.setStudentId(rs.getString("student_id"));
+                studentList.add(studentDto);
             }
         }
         return studentList;
     }
 
+
     //학생 student_ID로 학생 인증(로그인 용) 기능 만들기
     public Student authenicateStudent(String studentId) throws SQLException{
-        String sql = "SELECT * FROM student_id check";
+        String sql = "SELECT * FROM students where students_id = ?";
 
-        try(Connection conn = DatabaseUtil.getconnection());
+        try(Connection conn = DatabaseUtil.getconnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1,studentId);
+            ResultSet rs =pstmt.executeQuery();
+
+            if(rs.next()){
+                Student studentDTO = new Student();
+                studentDTO.setId(rs.getInt("id"));
+                studentDTO.setName(rs.getString("name"));
+                studentDTO.setStudentId(rs.getString("student_id"));
+                return studentDTO;
+            }
+        }
 
         // 학생이 정확한 학번을 입력하면 Student 객체가 만들어져서 리턴 됨
         // 학생이 잘못된 학번을 입력하면 null 값을 반환
@@ -54,8 +67,6 @@ public class StudentDAO {
         return null;
 
     }
-
-
-
+    
 
 }
